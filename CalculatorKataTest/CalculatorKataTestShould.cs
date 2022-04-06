50024fd43e619f33b6a8f1b8509f8d1c2ed3a452
Masa <masahzak98@gmail.com>
2022-04-07 01:26:20 +0300

    step1:create first test method

--- CalculatorKataTest/CalculatorKataTestShould.cs
+++ CalculatorKataTest/CalculatorKataTestShould.cs
@@ -6,7 +6,19 @@ namespace CalculatorKataTest
     public class CalculatorKataTestShould
     {
 
-        
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        public void SumOfNumbersHaveAtMostTwonumber(string num, int expectedResult)
+        {
+            var obj = new Program();
+
+            int result = obj.add(num);
+
+            Assert.Equal(expectedResult, result);
+
+        }
 
 
 
